fc1912b5f12e4876834bff27421d8e4f4f5b5f8e
FLUID11 <167190250+FLUID11@users.noreply.github.com>
2026-02-03 15:26:40 +0530

    added Media Details Pages

---
+++ MediaDetailsPage.js
@@ -0,0 +1,9 @@
+// Placeholder JS – logic comes later
+
+document.querySelector(".book-ticket").addEventListener("click", () => {
+  alert("Book Ticket Clicked");
+});
+
+document.querySelector(".trailer-btn").addEventListener("click", () => {
+  alert("Trailer Button Clicked");
+});
